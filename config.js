8bd042eb77b98f38fd9d114821b5d4684d06c1fb
Ultrman82 <exelcior99@gmail.com>
2019-10-04 17:37:06 -0700

    adding wildcard on cors

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://bautest:bau0099@ds145146.mlab.com:45146/confusion',
-    'facebook' : {
-        clientID : '869646549870360',
-        clientSecret: '3796eb8882371137d80ccd00ac833c51'  
-    }
-}
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://bautest:bau0099@ds145146.mlab.com:45146/confusion",
+  facebook: {
+    clientID: "",
+    clientSecret: ""
+  }
+};
--- routes/cors.js
+++ routes/cors.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-//const whitelist = ["*"];
-const whitelist = ['http://localhost:3000', 'https://localhost:3443', 'localhost:3001', 'http://localhost:3001',
-    "http://Bocal's iMac (46):3001"];
+const whitelist = ["*"];
+/* const whitelist = ['http://localhost:3000', 'https://localhost:3443', 'localhost:3001', 'http://localhost:3001',
+    "http://Bocal's iMac (46):3001"]; */
 var corsOptionsDelegate = (req, callback) => {
-    var corsOptions;    
-    if(whitelist.indexOf(req.header('Origin')) !== -1) {
-        corsOptions = { origin: true };
-    }
-    else {
-        corsOptions = { origin: false };
-    }
-    callback(null, corsOptions);
+  var corsOptions;
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true };
+  } else {
+    corsOptions = { origin: false };
+  }
+  callback(null, corsOptions);
 };
 
 exports.cors = cors();
